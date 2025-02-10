59ed202d4e8684099dd10a05725809fe41f5a33b
Noureddine Missoum <missoum.noureddine18@g.ens-kouba.dz>
2025-02-10 23:21:13 +0100

    Update Etudiant.java

--- Etudiant.java
+++ Etudiant.java
@@ -1,17 +1,14 @@
 package com.mycompany.projet_de_poo;
 
-
 public class Etudiant extends Personne {
     private String matricule;
     private double bacAverage;
     private String typeEtudiant;
 
-
     public Etudiant(String nom, String prenom, int age, String sexe, String matricule, double bacAverage) {
         super(nom, prenom, age, sexe);
         this.matricule = matricule;
         this.bacAverage = bacAverage;
-
         if (bacAverage > 14) {
             typeEtudiant = "ENS";
         } else {
@@ -19,17 +16,14 @@ public class Etudiant extends Personne {
         }
     }
 
-
     public String getMatricule() {
         return matricule;
     }
-
 
     public double getBacAverage() {
         return bacAverage;
     }
 
-    // Getter for typeEtudiant
     public String getTypeEtudiant() {
         return typeEtudiant;
     }
