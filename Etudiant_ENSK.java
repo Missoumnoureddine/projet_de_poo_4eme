aee04a803d09c8ba988734a3faaacc5341313954
Noureddine Missoum <missoum.noureddine18@g.ens-kouba.dz>
2025-02-11 00:30:37 +0100

    Update Etudiant_ENSK.java

--- Etudiant_ENSK.java
+++ Etudiant_ENSK.java
@@ -3,9 +3,9 @@ package com.mycompany.projet_de_poo;
 public class Etudiant_ENSK extends Etudiant {
     private String specialite;
 
-    public Etudiant_ENSK(String nom, String prenom, int age, String sexe, String matricule, double bacAverage) {
-        super(nom, prenom, age, sexe, matricule, bacAverage);
-        if (bacAverage < 16) {
+    public Etudiant_ENSK(String nom, String prenom, int age, String sexe, String matricule, double bac) {
+        super(nom, prenom, age, sexe, matricule, bac);
+        if (bac < 16) {
             specialite = "PEM";
         } else {
             specialite = "PES";
